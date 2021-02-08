27c52670b55413cc9302615184f4023f9d7cdaca
Ogilo G. D. Ouma <gogilo2003@hotmail.com>
2021-02-08 04:35:03 +0300

    Initial commit

--- src/WorldServiceProvider.php
+++ src/WorldServiceProvider.php
@@ -7,6 +7,6 @@ class WorldServiceProvider extends ServiceProvider{
     function boot()
     {
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        $this->loadfactoriesFrom(__DIR__.'/../database/factories');
+        $this->loadFactoriesFrom(__DIR__.'/../database/factories');
     }
 }
